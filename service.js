223a5416c7552d23f9bcd7b7815db238bcbcf61c
Sajjan Singh Mehta <sajjan.s.mehta@gmail.com>
2015-06-25 15:42:59 -0700

    temporary fix to smiles regex

--- service.js
+++ service.js
@@ -88,7 +88,8 @@ angular.module('wohlgemuth.mgf.parser', []).
          */
         function inspectFields(match, spectra) {
             var regexInchIKey = /.*([A-Z]{14}-[A-Z]{10}-[A-Z,0-9])+.*/;
-            var regexSmiles = /^([^J][0-9BCOHNSOPrIFla@+\-\[\]\(\)\\\/%=#$,.~&!]{6,})$/;
+            //var regexSmiles = /^([^J][0-9BCOHNSOPrIFla@+\-\[\]\(\)\\\/%=#$,.~&!]{6,})$/;
+            var regexSmiles = /^([^J][0-9A-Za-z@+\-\[\]\(\)\\\/%=#$,.~&!]{6,})$/;
 
             //if we contain an inchi key in any propterty of this field
             if(regexInchIKey.exec(match[2])){
